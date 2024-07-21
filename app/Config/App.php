4ce66ee5ddf87e68d87d69cbd8da4a080fc5dd2f
Nurul Istika <nurul@example.com>
2024-07-22 01:05:56 +0700

    Update File

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/SKRIPSI/Sistem-Pakar-Depresi';
+    public string $baseURL = 'http://localhost/PROJECT-SKRIPSI/Sistem-Pakar-Depresi';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
